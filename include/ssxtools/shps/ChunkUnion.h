15521378bd25a52b72258c8a105c53575889abb9
modeco80 <lily.modeco80@protonmail.ch>
2021-07-21 18:28:21 -0400

    [SHPS] Refactor the memory of the tagged union to use some wrappers
    I found this curious oddity, originally someone did
    (0+...+sizeof(T)) however it works just fine as (...+sizeof(T))

--- include/ssxtools/shps/ChunkUnion.h
+++ include/ssxtools/shps/ChunkUnion.h
@@ -9,6 +9,25 @@
 
 namespace ssxtools::shps {
 
+	namespace detail {
+		/**
+		 * Returns the combined size of however many objects you put into this template.
+		 * \code
+		 * constexpr auto AllTheSizes = CombinedSizeOf<int, std::string, unsigned long, short, double>();
+		 * \endcode
+		 */
+		template<class... T>
+		consteval std::size_t CombinedSizeOf() {
+			return (...+sizeof(T));
+		}
+
+		/**
+		 * Helper type.
+		 */
+		template<class... T>
+		using UnionMemory = std::uint8_t[CombinedSizeOf<T...>()];
+	}
+
 	/**
 	 * A tagged container for chunks,
 	 * which automatically handles reading and accessing of the proper chunk data type(s).
@@ -52,18 +71,28 @@ namespace ssxtools::shps {
 		GeneralizedType GetGeneralType() const;
 
 		/**
-		 * Get a given fully defined chunk type. Useful for visiting, like:
+		 * Get a given fully defined chunk type. Useful for visiting this chunk, like:
 		 * \code
 		 * if(chunkUnion.GetGeneralType() == ChunkUnion::GeneralizedType::Image) {
 		 * 	// This is a shape header, let's visit it!
-		 * 	ShapeHeaderChunk& shape = chunkUnion.GetChunk<ShapeHeaderChunk>();
-		 * 	// now we can visit the shape header!
+		 * 	const ShapeHeaderChunk& shape = chunkUnion.GetChunk<ShapeHeaderChunk>();
+		 * 	auto n = shape.width; // pull out some information
 		 * }
 		 * \endcode
-		 *
 		 */
 		template<class T>
 		inline T& GetChunk() {
+			if constexpr(std::is_same_v<T, structures::ShapeHeaderChunk>) {
+				assert(GetGeneralType() == GeneralizedType::Image || GetGeneralType() == GeneralizedType::Palette);
+				return a.shape;
+			} else if constexpr(std::is_same_v<T, structures::FullNameChunk>) {
+				assert(GetGeneralType() == GeneralizedType::FullName);
+				return b.fullName;
+			}
+		}
+
+		template<class T>
+		inline const T& GetChunk() const {
 			if constexpr(std::is_same_v<T, structures::ShapeHeaderChunk>) {
 				assert(GetGeneralType() == GeneralizedType::Image || GetGeneralType() == GeneralizedType::Palette);
 				return a.shape;
@@ -80,20 +109,22 @@ namespace ssxtools::shps {
 		static GeneralizedType GeneralizedTypeFromChunkType(structures::Chunk::Type type);
 
 		// Helper to shorten the transform switch.
-		// Automatically
+		// Automatically initalizes the right member
 		template<core::Stream Stream, class T>
-		inline bool TransformUnion(T& t, Stream& stream) {
-			// This placement new should only be done for reading.
+		inline bool TransformUnion(T& union_member, Stream& stream) {
+			// Placement-new (calling the constructor)
+			// only if we need to.
 			if constexpr(Stream::IsReadStream())
-				new(&t) T();
-			SSXTOOLS_CATCH_ERROR(stream.template Other(t));
+				new(&union_member) T();
+
+			SSXTOOLS_CATCH_ERROR(stream.template Other(union_member));
 		}
 
 		// Tagged union. The generalized type member tags which member is active.
 		// Probably a bit wasteful, but whatever.
 		union {
 			// the whole memory of the union
-			std::uint8_t memory[sizeof(structures::ShapeHeaderChunk) + sizeof(structures::FullNameChunk)];
+			detail::UnionMemory<structures::ShapeHeaderChunk, structures::FullNameChunk> memory;
 
 			// accessor used when header.type is a image
 			struct {
