9685cc90417a9fa80f58e0a76f8c368ea6d17a92
modeco80 <lily.modeco80@protonmail.ch>
2023-09-04 04:47:11 -0400

    format: Add BX core types (for later on)

---
+++ projects/libraries/format/include/ssxtools/BxTypes.hpp
@@ -0,0 +1,37 @@
+//! BX types
+
+#pragma once
+#include <ssxtools/Types.hpp>
+
+namespace ssxtools::format {
+
+	struct tVec2 {
+		float x;
+		float y;
+	};
+
+	struct tVec3 {
+		float x;
+		float y;
+		float z;
+	};
+
+	struct tVec4 {
+		float x;
+		float y;
+		float z;
+		float w;
+	};
+
+	struct tMat4 {
+		float mats[4][4];
+	};
+
+	struct tARGB {
+		float a;
+		float r;
+		float g;
+		float b;
+	};
+
+}
